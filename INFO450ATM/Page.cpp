fc08e1598cc701aeb4a0bff9d72639790c27f13e
DannyBobby <drg8705@gmail.com>
2014-12-14 09:27:29 -0500

    Commented Page.h and Page.cpp

--- INFO450ATM/Page.cpp
+++ INFO450ATM/Page.cpp
@@ -9,22 +9,22 @@ Page::~Page()
 {
 }
 
-void Page::AddLine(string lineText)
-{
-	if (collectionOfLines.size() < (maximumNumberOfLines + 1))
-	{
-		collectionOfLines.push_back(lineText);
-	}	
-}
-
+// Return the string at the specified "line"
+// or index within the vector
 string Page::GetLine(unsigned int lineNumber)
 {
+	// Check to see if it is possible for there to be
+	// anything "written" on this "lineNumber" by seeing
+	// if the lineNumber is less than the size of the vector.
 	if (lineNumber < collectionOfLines.size())
 	{
+		// If so, return the string stored at this index
 		return collectionOfLines[lineNumber];
 	}
+	// If it isn't possible for there to be anything there...
 	else
 	{
+		// Return the empty string
 		return "";
 	}
 }
@@ -34,7 +34,21 @@ unsigned int Page::GetMaximumNumberOfLines()
 	return this->maximumNumberOfLines;
 }
 
+// Return the number of lines currently "written"
+// to the page (the size of the vector).
 unsigned int Page::GetNumberOfLines()
 {
 	return this->collectionOfLines.size();
+}
+
+// Add a line to the page
+void Page::AddLine(string lineText)
+{
+	// If the number of lines on the page has not yet reached
+	// the maximum allowable number...
+	if (collectionOfLines.size() < (maximumNumberOfLines + 1))
+	{
+		// Add the new line to the page.
+		collectionOfLines.push_back(lineText);
+	}
 }
--- INFO450ATM/Page.h
+++ INFO450ATM/Page.h
@@ -1,3 +1,5 @@
+// This object is used in the construction of reports - Transaction/Transfer History
+
 #ifndef PAGE_H
 #define PAGE_H
 
@@ -10,17 +12,28 @@ using namespace std;
 class Page
 {
 private:
+	// Each page has a maximum number of lines so that 
+	// it all fits in one console window without scrolling
 	unsigned int maximumNumberOfLines = 10;
+
+	// This vector stores individual strings which make up 
+	// the lines displayed on the page.
 	vector<string> collectionOfLines;
 
 public:
 	Page();
 	~Page();
 
-	void AddLine(string lineText);
+	// Get functions
 	unsigned int GetMaximumNumberOfLines();
 	string GetLine(unsigned int lineNumber);
+	// This is different from GetMaximumNumberOfLines()
+	// because it returns the number of lines which the 
+	// page currently has "written on" it.
 	unsigned int GetNumberOfLines();
+
+	// Add a line to the page
+	void AddLine(string lineText);
 };
 
 #endif
